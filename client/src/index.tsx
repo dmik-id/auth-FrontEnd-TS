aacdbc77d7044d159aa402c496a4e0a42f2ef5e9
dmik-id <dmitriev.i@phystech.edu>
2021-09-21 17:49:23 +0300

    gql

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React, {createContext} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Store from "./store/store";
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 interface State {
     store: Store,
@@ -13,12 +14,21 @@ export const Context = createContext<State>({
     store,
 })
 
+const client = new ApolloClient({
+    uri: 'http://localhost:5000/graphql' ,
+    cache: new InMemoryCache()
+
+})
+
 ReactDOM.render(
-    <Context.Provider value={{
-        store
-    }}>
-        <App />
-    </Context.Provider>,
+
+    <ApolloProvider client={client}>
+        <Context.Provider value={{
+            store
+        }}>
+            <App />
+        </Context.Provider>,
+    </ApolloProvider>,
   document.getElementById('root')
 );
 
